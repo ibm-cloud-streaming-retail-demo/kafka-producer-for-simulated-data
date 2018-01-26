d3526c16aa99c03aa21a2c8fbd98b7a61d4eddc1
christopher snow <chsnow123@gmail.com>
2018-01-26 08:01:26 +0000

    handle paths with spaces

--- bin/run_locally.sh
+++ bin/run_locally.sh
@@ -12,15 +12,15 @@ PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 # retrieve the file path for the message hub credentials
 export VCAP_SERVICES_FILE="${PROJECT_DIR}/etc/message_hub_vcap.json"
 
-if [[ ! -f $VCAP_SERVICES_FILE ]]; then
-   echo "ERROR: Couldn't find $VCAP_SERVICES_FILE"
+if [[ ! -f "$VCAP_SERVICES_FILE" ]]; then
+   echo "ERROR: Couldn't find '$VCAP_SERVICES_FILE'"
    exit -1
 fi
 
 # retrieve the json credentials
-export VCAP_SERVICES="{$(cat ${VCAP_SERVICES_FILE})}"
+export VCAP_SERVICES="{$(cat "${VCAP_SERVICES_FILE}")}"
 export PORT=12345
 export TRANSACTIONS_TOPIC=transactions_load
 export CF_INSTANCE_INDEX=0
 
-python3 index.py
+python3 "$PROJECT_DIR/index.py"
